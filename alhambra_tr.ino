949fb3a748a229ca024e42a030c6e5236774478e
Tobias Bielohlawek <tobi@soundcloud.com>
2013-03-23 18:57:10 +0100

    dummy

--- alhambra_tr.ino
+++ alhambra_tr.ino
@@ -1,7 +1,7 @@
 #include "RGBdriver.h"
 
-#define CLK 1//pins definitions for the driver
-#define DIO 2
+#define CLK 2//pins definitions for the driver
+#define DIO 3
 
 RGBdriver Driver(CLK,DIO);
 
@@ -9,21 +9,24 @@ int pause;
 
 void setup()
 {
+  pinMode(7, OUTPUT);
 }
 
 void loop()
 {
   Driver.begin();
-  Driver.SetColor(255,230,230);
+  Driver.SetColor(255,0,0);
   Driver.end();
+  digitalWrite(7, HIGH);
 
   pause = random(5000);
   delay(pause);
 
 
   Driver.begin();
-  Driver.SetColor(0,0,0);
+  Driver.SetColor(0,230,230);
   Driver.end();
+  digitalWrite(7, LOW);
 
   pause = random(2000);
   delay(pause);
