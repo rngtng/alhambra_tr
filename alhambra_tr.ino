4e929e78037f244e350327d73d4e961e07f1c5b8
Tobias Bielohlawek <tobi@soundcloud.com>
2013-03-23 14:08:34 +0100

    random

--- alhambra_tr.ino
+++ alhambra_tr.ino
@@ -1,80 +1,30 @@
 #include "RGBdriver.h"
-#include <Ultrasonic.h>
-
-#include <SerialLCD.h>
-#include <SoftwareSerial.h> //this is a must
 
 #define CLK 1//pins definitions for the driver
 #define DIO 2
 
-#define DISTANCE_SENSOR 7
-#define SOUND_SENSOR A0
-
-SerialLCD slcd(11,12);
 RGBdriver Driver(CLK,DIO);
-Ultrasonic ultrasonic(DISTANCE_SENSOR);
 
-int distance;
-int x;
-int last_x;
-
-int m;
+int pause;
 
 void setup()
 {
-  pinMode(SOUND_SENSOR, INPUT);
-
-  slcd.begin();
-  slcd.backlight();
-  slcd.clear();
-  slcd.setCursor(0,0);
-  slcd.print("The distance:");
 }
 
 void loop()
 {
-  // ultrasonic.MeasureInCentimeters();
-  // distance = ultrasonic.RangeInCentimeters - 40;
-
-  x = analogRead(SOUND_SENSOR);
-
-  slcd.setCursor(0,1);
-  slcd.print("    ");
-  slcd.setCursor(0,1);
-  slcd.print(x,DEC);
-
-  slcd.setCursor(5,1);
-  slcd.print("    ");
-  slcd.setCursor(5,1);
-  slcd.print(m,DEC);
-
-  // if(distance < 0) {
-  //   distance = 0;
-  // }
-
-  // if(distance > 100) {
-  //   x = 0;
-  // }
-  // else {
-  //   x = 255 - (255 * distance / 100);
-  // }
-
-  if(last_x < x) {
-    x = ( 5 * last_x + x) / 6;
-  }
-
-  if(last_x > x) {
-    x = ( 5 * last_x + x) / 6;
-  }
-
-  if(m < x) {
-    m = x;
-  }
-
- int j = 255 - x;
   Driver.begin();
-  Driver.SetColor(j,j,j);
+  Driver.SetColor(255,230,230);
   Driver.end();
-  delay(10);
-  last_x = x;
+
+  pause = random(5000);
+  delay(pause);
+
+
+  Driver.begin();
+  Driver.SetColor(0,0,0);
+  Driver.end();
+
+  pause = random(2000);
+  delay(pause);
 }
